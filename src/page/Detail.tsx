478fe42e41bbaa81750620c3b9aa464dd3b532ab
paran000ia <reyodzaki20100612@gmail.com>
2026-06-25 15:55:10 +0500

    sssss

--- src/page/Detail.tsx
+++ src/page/Detail.tsx
@@ -1,15 +1,15 @@
 import Header from "../components/Header";
 import Footer from "../components/Footer";
 import Episode1 from "../components/Episode1"
-import LatestEpisode from "../components/LatestEpisode";
 import PodOfCastApp from "../components/PodOfCastApp";
+import RecentEpisodes from "../components/RecentEpisodes";
 
 export default function Home() {
     return (
     <>
       <Header />
             <Episode1 />
-            <LatestEpisode />
+            <RecentEpisodes />
             <PodOfCastApp />
       <Footer />
     </>
